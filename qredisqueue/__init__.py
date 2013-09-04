12411f49dae0b77820f338b15b484f8aa059e525
Vital Belikov <vital@qwe.lv>
2013-09-04 20:05:04 +0300

    redis in args

--- qredisqueue/__init__.py
+++ qredisqueue/__init__.py
@@ -2,9 +2,13 @@ import redis
 
 class RedisQueue(object):
     """Simple Queue with Redis Backend"""
-    def __init__(self, name, namespace='queue', **redis_kwargs):
+    def __init__(self, name, namespace='queue', redis_db=None, **redis_kwargs):
         """The default connection parameters are: host='localhost', port=6379, db=0"""
-        self.__db= redis.Redis(**redis_kwargs)
+        if redis_db:
+            self.__db = redis_db
+        else:
+            self.__db = redis.Redis(**redis_kwargs)
+
         self.key = '%s:%s' % (namespace, name)
 
     def qsize(self):
